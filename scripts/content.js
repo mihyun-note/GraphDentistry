bbcdcd8eddf7bc3acb75ebe30e0515d0a8b663e2
미현 <83504363+mihyun-note@users.noreply.github.com>
2022-10-22 02:10:54 +0900

    Update content.js
    insert text!

--- scripts/content.js
+++ scripts/content.js
@@ -1,5 +1,14 @@
-const image = document.querySelector("img");
+//img 태그 인식
+const image = document.querySelectorAll("img");
 
-if (image) {
-  console.log("image 태그 발견!");
+//이미지 아래에 추가할 <p>태그 생성
+const text_ = [];
+
+for (var i = 0; i < image.length; i++) {
+  //text_.classList.add("color-secondary-text", "type--caption");
+  text_[i] = document.createElement("p");
+  text_[i].textContent = image[i].alt + `: text add`;
+  console.log(text_[i], i, image[i]);
+  //텍스트 삽입
+  image[i].insertAdjacentElement("afterend", text_[i]);
 }
