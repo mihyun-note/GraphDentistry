1a3007c002ba4120373d13737ee304cef184eff0
Uddiyam <97297404+Uddiyam@users.noreply.github.com>
2022-12-13 21:56:38 +0900

    사이즈 조정

--- scripts/content.js
+++ scripts/content.js
@@ -5,6 +5,7 @@ let img_link = [];
 
 for (let i = 0; i < img.length; i++) {
   img_link[i] = img[i].src;
+  console.log(img_link[i]);
 }
 
 const ErrorTitle = [];
@@ -43,81 +44,84 @@ resData = async (link, i) => {
     .then((response) => response.json())
     .then((RESdata) => {
       console.log(RESdata);
-      chartStatus = RESdata.status;
-      if (chartStatus == "NOT_BAR") {
-        ErrorTitle[i].textContent = "막대그래프가 아닙니다";
-        img[i].insertAdjacentElement("afterend", ErrorTitle[i]);
-      } else if (chartStatus == "NO_ERROR") {
-        ErrorTitle[i].textContent = "교정할 오류가 없습니다";
-        img[i].insertAdjacentElement("afterend", ErrorTitle[i]);
-      } else if (chartStatus == "ERROR_FOUND") {
-        ex[i] = document.createElement("p");
-        ex[i].textContent = "오류 교정 그래프";
-        ex[i].id = "addP";
-        img[i].insertAdjacentElement("afterend", ex[i]);
+      if (img[i].offsetWidth > 300) {
+        console.log("통과");
+        chartStatus = RESdata.status;
+        if (chartStatus == "NOT_BAR") {
+          ErrorTitle[i].textContent = "";
+          img[i].insertAdjacentElement("afterend", ErrorTitle[i]);
+        } else if (chartStatus == "NO_ERROR") {
+          ErrorTitle[i].textContent = "교정할 오류가 없습니다";
+          img[i].insertAdjacentElement("afterend", ErrorTitle[i]);
+        } else if (chartStatus == "ERROR_FOUND") {
+          ex[i] = document.createElement("p");
+          ex[i].textContent = "오류 교정 그래프";
+          ex[i].id = "addP";
+          img[i].insertAdjacentElement("afterend", ex[i]);
 
-        chartDiv[i] = document.createElement("div");
-        chartDiv[i].id = "chartDiv";
-        chart[i] = document.createElement("canvas");
-        chart[i].id = "chart" + i;
-        chart[i].width = img[i].width;
-        chart[i].height = img[i].height;
-        chartDiv[i].appendChild(chart[i]);
-        ex[i].insertAdjacentElement("afterend", chartDiv[i]);
-        chartX = RESdata.xlabels;
-        chartY = [RESdata.ylabels[-1], RESdata.ylabels[0]];
-        chartCount = RESdata.chartDataList.length;
-        chartDATAS = RESdata.chartDataList;
-        chartERROR = RESdata.errorChartList;
+          chartDiv[i] = document.createElement("div");
+          chartDiv[i].id = "chartDiv";
+          chart[i] = document.createElement("canvas");
+          chart[i].id = "chart" + i;
+          chart[i].width = img[i].width;
+          chart[i].height = img[i].height;
+          chartDiv[i].appendChild(chart[i]);
+          ex[i].insertAdjacentElement("afterend", chartDiv[i]);
+          chartX = RESdata.xlabels;
+          chartY = [RESdata.ylabels[-1], RESdata.ylabels[0]];
+          chartCount = RESdata.chartDataList.length;
+          chartDATAS = RESdata.chartDataList;
+          chartERROR = RESdata.errorChartList;
 
-        graph_redraw(i, chartX, chartY);
-        addData(myChart, chartCount, chartDATAS);
+          graph_redraw(i, chartX, chartY);
+          addData(myChart, chartCount, chartDATAS);
 
-        ErrorTitle[i] = document.createElement("p");
-        ErrorTitle[i].textContent = `오류가 존재합니다`;
-        ErrorTitle[i].id = "addText";
-        chartDiv[i].insertAdjacentElement("afterend", ErrorTitle[i]);
+          ErrorTitle[i] = document.createElement("p");
+          ErrorTitle[i].textContent = `오류가 존재합니다`;
+          ErrorTitle[i].id = "addText";
+          chartDiv[i].insertAdjacentElement("afterend", ErrorTitle[i]);
 
-        for (let j = 0; j < chartERROR.length; j++) {
-          let Bcolor =
-            "#" +
-            ToHex(chartERROR[j].color[2]) +
-            ToHex(chartERROR[j].color[1]) +
-            ToHex(chartERROR[j].color[0]);
+          for (let j = 0; j < chartERROR.length; j++) {
+            let Bcolor =
+              "#" +
+              ToHex(chartERROR[j].color[2]) +
+              ToHex(chartERROR[j].color[1]) +
+              ToHex(chartERROR[j].color[0]);
 
-          errBarText[j] =
-            "<span style = 'color:" +
-            Bcolor +
-            "; '>" +
-            "●" +
-            "</span>" +
-            "<span>" +
-            ' "' +
-            "<span style = 'font-weight: bold;'>" +
-            chartERROR[j].legend +
-            "</span>" +
-            '" 범례의 "' +
-            "<span style = 'font-weight: bold;'>" +
-            chartERROR[j].xlabel +
-            "</span>" +
-            '"막대에서 명시된 텍스트보다 "' +
-            "<span style = 'font-weight: bold;'>" +
-            chartERROR[j].errorDiff.toFixed(1) +
-            "</span>" +
-            '"만큼 차이가 존재하는 것으로 추정됩니다' +
-            "<span/><br/>";
+            errBarText[j] =
+              "<span style = 'color:" +
+              Bcolor +
+              "; '>" +
+              "●" +
+              "</span>" +
+              "<span>" +
+              ' "' +
+              "<span style = 'font-weight: bold;'>" +
+              chartERROR[j].legend +
+              "</span>" +
+              '" 범례의 "' +
+              "<span style = 'font-weight: bold;'>" +
+              chartERROR[j].xlabel +
+              "</span>" +
+              '"막대에서 명시된 텍스트보다 "' +
+              "<span style = 'font-weight: bold;'>" +
+              chartERROR[j].errorDiff.toFixed(1) +
+              "</span>" +
+              '"만큼 차이가 존재하는 것으로 추정됩니다' +
+              "<span/><br/>";
+          }
+
+          errBar[i] = document.createElement("p");
+
+          for (let q = 0; q < chartERROR.length; q++) {
+            errBar[i].innerHTML += errBarText[q];
+          }
+          errBar[i].id = "addErrText";
+          ErrorTitle[i].insertAdjacentElement("afterend", errBar[i]);
+        } else {
+          ErrorTitle[i].textContent = "그 외 오류";
+          img[i].insertAdjacentElement("afterend", ErrorTitle[i]);
         }
-
-        errBar[i] = document.createElement("p");
-
-        for (let q = 0; q < chartERROR.length; q++) {
-          errBar[i].innerHTML += errBarText[q];
-        }
-        errBar[i].id = "addErrText";
-        ErrorTitle[i].insertAdjacentElement("afterend", errBar[i]);
-      } else {
-        ErrorTitle[i].textContent = "그 외 오류";
-        img[i].insertAdjacentElement("afterend", ErrorTitle[i]);
       }
     });
 };
